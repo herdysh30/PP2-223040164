03363b88059456282a536376735b69d6a08669bc
herdysh30 <herdy.syaban@gmail.com>
2024-10-14 10:23:14 +0700

    Pertemuan 4 :EventListener

---
+++ pertemuan4/ButtonExample.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan4;
+
+/**
+ *
+ * @author Herdy
+ */
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ButtonExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Button Example");
+        JButton button = new JButton("Click Me");
+        
+        //Menambahkan ActionListener ke JButton
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Button Clicked!");
+            }
+        });
+        
+        button.setBounds(50,50,150,30);
+        frame.add(button);
+        frame.setSize(300,200);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
